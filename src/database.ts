3a4d7d21472532574f701e816519b067f24c4b58
Mike Burke <mike@jovenhealth.com>
2023-01-30 18:34:17 -0700

    Fix copy-paste error in database URL

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@
 // Creates a connection to the application's database
 import { PrismaClient } from '.prisma/client'
 
-const DATABASE_URL = `postgresql://postgres:postgres@${process.env.HOSTNAME}:${process.env.HOSTNAME}/juno_dev`
+const DATABASE_URL = `postgresql://postgres:postgres@${process.env.HOSTNAME}:${process.env.PORT}/juno_dev`
 
 // Exports the Prisma client that can be used by any file that imports it.
 export const prismaClient = new PrismaClient({
