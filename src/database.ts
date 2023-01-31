bf3caf0a6e6e4ee0066194dbc5943c0fcc924a16
Mike Burke <mike@jovenhealth.com>
2023-01-30 19:26:04 -0700

    Undo overriding DATABASE_URL

--- src/database.ts
+++ src/database.ts
@@ -2,13 +2,5 @@
 // Creates a connection to the application's database
 import { PrismaClient } from '.prisma/client'
 
-const DATABASE_URL = `postgresql://postgres:postgres@${process.env.HOSTNAME}:${process.env.PORT}/juno_dev`
-
 // Exports the Prisma client that can be used by any file that imports it.
-export const prismaClient = new PrismaClient({
-  datasources: {
-    db: {
-      url: DATABASE_URL
-    }
-  }
-})
+export const prismaClient = new PrismaClient()
