678e4bf2683a9e23e6d0cb8d295dc0646e8d180f
Mike Burke <mike@jovenhealth.com>
2023-02-21 13:50:58 -0700

    Set up Cypress E2E testing and install example project

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // The rest of the Cypress config options go here...
+  projectId: 'rr9qyh',
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    }
+  }
+}
